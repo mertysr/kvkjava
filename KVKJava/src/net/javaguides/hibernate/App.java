6ac1e4ffce2dcec4db80295de271e154a9365e1f
Mert Yasar Karadag <35815457+mertysr@users.noreply.github.com>
2020-01-02 18:05:28 +0200

    Initialized

---
+++ KVKJava/src/net/javaguides/hibernate/App.java
@@ -0,0 +1,101 @@
+package net.javaguides.hibernate;
+//import java.io.Console;
+//import org.hibernate.Session;
+//import org.hibernate.SessionFactory;
+//import org.hibernate.Transaction;
+//import org.hibernate.cfg.Configuration;
+import net.javaguides.hibernate.model.Address;
+import net.javaguides.hibernate.model.Mobile;
+import net.javaguides.hibernate.model.User;
+import net.javaguides.hibernate.model.Vehicle;
+//import net.javaguides.hibernate.util.HibernateUtil;
+public class App {
+public static void main(String[] args) {
+	User usr=new User("Mert");
+	Address adr=new Address("street","city",usr);
+	Mobile mbl=new Mobile("Huawei","p9",usr);
+	usr.setAddress(adr);
+	User user=new User();
+	User user2=new User();
+	user2.setUserName("Dev");
+	user.setUserName("Ankit");
+	Address address= new Address();
+	address.setStreet("sector 15");
+	address.setCity("noida");
+	Address address2=new Address();
+	address2.setCity("Muzaffarpur");
+	address2.setStreet("sahebganj");
+	Vehicle veh=new Vehicle();
+	veh.setName("car");
+	Vehicle vehicle=new Vehicle();
+	vehicle.setName("jeep");
+	Vehicle vehicle2= new Vehicle();
+	vehicle2.setName("Bike");
+	Vehicle vehicle3= new Vehicle();
+	vehicle3.setName("Bus");
+	Vehicle vehicle4=new Vehicle();
+	vehicle4.setName("cycle");
+	Vehicle vehicle5= new Vehicle();
+	vehicle5.setName("Truck");
+	Mobile mobile =new Mobile();
+	mobile.setBrand("sony");
+	mobile.setModel("xperia z3");
+	Mobile mobile2 = new Mobile();
+	mobile2.setBrand("Redmi");
+	mobile2.setModel("Note 5 pro");
+	Mobile mobile3 = new Mobile();
+	mobile3.setBrand("Nokia");
+	mobile3.setModel("7 plus");
+	user.setAddress(address);
+	user2.setAddress(address2);
+	address.setUser(user);
+	address2.setUser(user2);
+	user.getMobile().add(mobile);
+	user.getMobile().add(mobile2);
+	
+	mobile.setUser(user);
+	mobile2.setUser(user);
+	user2.getMobile().add(mobile3);
+	mobile3.setUser(user2);
+	user.getVehicle().add(veh);
+	user.getVehicle().add(vehicle);
+	user.getVehicle().add(vehicle2);
+	veh.getUser().add(user);
+	vehicle.getUser().add(user);
+	vehicle2.getUser().add(user);
+	user2.getVehicle().add(vehicle3);
+	user2.getVehicle().add(vehicle4);
+	user2.getVehicle().add(vehicle5);
+	vehicle3.getUser().add(user2);
+	vehicle4.getUser().add(user2);
+	vehicle5.getUser().add(user2);
+
+	/*Transaction transaction =null;
+	try(Session session=HibernateUtil.getSessionFactory().openSession()){
+		System.out.println("dada");
+		transaction = session.beginTransaction();
+		session.save(user);
+		session.save(user2);
+		transaction.commit();
+	}catch (Exception e) {
+		if (transaction!=null) {
+			transaction.rollback();
+		}
+	}
+	Mobile addresss=null;
+	Mobile addressss=null;
+	try(Session session=HibernateUtil.getSessionFactory().openSession()){
+		transaction = session.beginTransaction();
+
+		addresss = session.get(Mobile.class, 3);
+		addressss = session.get(Mobile.class, 10);
+		session.delete(addresss);
+		session.delete(addressss);
+		transaction.commit();
+	}catch (Exception e) {
+		if (transaction!=null) {
+			transaction.rollback();
+		}
+	}*/
+}}
+
---
+++ KVKJava/src/net/javaguides/hibernate/model/Address.java
@@ -0,0 +1,45 @@
+package net.javaguides.hibernate.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+@Entity
+public class Address {
+@Id @GeneratedValue
+private int id;
+private String street;
+private String city;
+@OneToOne(mappedBy="address")
+private User user;
+public Address() {}
+public Address(String street, String city,User user) {
+	this.street = street;
+	this.city = city;
+	this.user = user;
+}
+public String getStreet() {
+	return street;
+}
+public void setStreet(String street) {
+	this.street = street;
+}
+public String getCity() {
+	return city;
+}
+public User getUser() {
+	return user;
+}
+public void setUser(User user) {
+	this.user = user;
+}
+public void setCity(String city) {
+	this.city = city;
+}
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+}
